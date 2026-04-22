7f05628ba0ba1241426a25d98a9d125807a765ba
nozzip <aiko.zip@hotmail.com>
2026-04-22 15:07:37 -0300

    Updates

--- sw.js
+++ sw.js
@@ -1,18 +1,25 @@
-const CACHE_NAME = 'aefip-cache-v5';
+const CACHE_NAME = 'aefip-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
 self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', event => {
-  // Must have a fetch handler for PWA installability
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
   );
 });
