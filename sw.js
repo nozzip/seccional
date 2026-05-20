411acc1e55436edcb2f50f6ecdfa45f11a89f3fe
nozzip <aiko.zip@hotmail.com>
2026-05-20 11:19:43 -0300

    Updates

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// Build Timestamp: 20/5/2026, 09:57:02
-// Unique ID: 1779281822015
-const CACHE_NAME = 'aefip-cache-1779281822015';
+// Build Timestamp: 20/5/2026, 11:19:41
+// Unique ID: 1779286781701
+const CACHE_NAME = 'aefip-cache-1779286781701';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
